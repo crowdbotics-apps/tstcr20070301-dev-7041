52cc0a24582f7eabe3b64185e62fc7b90a345eeb
Crowdbotics <team@crowdbotics.com>
2020-07-16 16:25:38 +0000

    Update components

--- src/features/BlankScreen58303/screens/blank.js
+++ src/features/BlankScreen58303/screens/blank.js
@@ -18,7 +18,7 @@ export class _Blank extends React.Component {
     title: "Blank".toUpperCase()
   }
 
-  state = {}
+  state = { Input_10: "" }
 
   render = () => (
     <View
@@ -128,7 +128,42 @@ export class _Blank extends React.Component {
             borderRadius: 0,
             backgroundSize: "auto"
           }}
-        />
+        >
+          <Input
+            placeholder="Sample text input placeholder"
+            editable={true}
+            textStyle={{ fontSize: 20, color: "#000000" }}
+            style={{
+              width: "100%",
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 5,
+              marginBottom: 5,
+              paddingLeft: 5,
+              paddingRight: 5,
+              paddingTop: 5,
+              paddingBottom: 5,
+              overflow: "visible",
+              textAlign: "left",
+              verticalAlign: "baseline",
+              fontSize: 20,
+              color: "#000000",
+              backgroundColor: "#ffffff",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0
+            }}
+            value={this.state.Input_10}
+            onChangeText={nextValue => this.setState({ Input_10: nextValue })}
+          />
+        </View>
         <View
           style={{
             width: "100%",
